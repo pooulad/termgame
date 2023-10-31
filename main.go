21b36948d51fbbd88b0c1eee0aaf119c376ad798
pooulad <poooooladi@gmail.com>
2023-10-31 20:52:00 +0330

    clean main file

--- main.go
+++ main.go
@@ -67,29 +67,7 @@ func (g *game) render() {}
 func main() {
 	width := 80
 	height := 18
-	level := make([][]byte, 18)
 
-	for h := 0; h < height; h++ {
-		for w := 0; w < width; w++ {
-			level[h] = make([]byte, width)
-		}
-	}
-	for h := 0; h < height; h++ {
-		for w := 0; w < width; w++ {
-			if w == width-1 {
-				level[h][w] = wall
-			}
-			if w == 0 {
-				level[h][w] = wall
-			}
-			if h == 0 {
-				level[h][w] = wall
-			}
-			if h == height-1 {
-				level[h][w] = wall
-			}
-		}
-	}
 	buf := new(bytes.Buffer)
 	for h := 0; h < height; h++ {
 		for w := 0; w < width; w++ {
