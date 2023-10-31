3ff73c193e814969fb1b51554eb14daf996077f5
pooulad <poooooladi@gmail.com>
2023-10-31 19:40:37 +0330

    create an basic board

--- main.go
+++ main.go
@@ -7,5 +7,18 @@ func (g *game) update() {}
 func (g *game) render() {}
 
 func main() {
+	width := 20
+	height := 20
+	level := make([][]byte, 20)
 
+	for h := 0; h < height; h++ {
+		for w := 0; w < width; w++ {
+			level[h] = make([]byte, width)
+		}
+	}
+	for h := 0; h < height; h++ {
+		for w := 0; w < width; w++ {
+			level[h][w] = 0
+		}
+	}
 }
